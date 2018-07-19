4afb53346ff62c2fdbc700fb13d9595c32b90e12
Stephen John Raymundo <bayan.mahal@live.com>
2018-07-19 14:51:33 +0800

    Don't Delete Custom_Btn_Layout_bg Color Primary is changed

---
+++ app/src/main/java/com/hilagangluzon/foodhub/Profile.java
@@ -0,0 +1,15 @@
+package com.hilagangluzon.foodhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_profile);
+    }
+
+
+}
